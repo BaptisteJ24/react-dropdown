3727a0ad7adbf921bd36c4c6ea19e5372b59b619
BaptisteJ24 <baptistejan@outlook.fr>
2023-09-19 19:19:08 +0200

    publish dropdown

--- demo/App.jsx
+++ demo/App.jsx
@@ -1,10 +1,165 @@
 import Dropdown from "../src/components/dropdown";
+import "./style.css";
 
 function App() {
   return (
-    <div>
-      <h1>React Dropdown Component Package</h1>
-      <Dropdown classname={"oui"} />
+    <div className="demo">
+      <h1>@baptistej24/react-dropdown</h1>
+      <h2>Dropdown Component Demo</h2>
+      <ul className="demo__list">
+        <li className="demo__list__item">
+          <p>Default Dropdown :</p>
+          <Dropdown />
+        </li>
+        <li className="demo__list__item">
+          <p>Custom placeholder Dropdown</p>
+          <Dropdown placeholder={"Custom Placeholder"} />
+        </li>
+        <li className="demo__list__item">
+          <p>Custom data Dropdown</p>
+          <Dropdown data={["Value 1", "Value 2", "Value 3"]} />
+        </li>
+        <li className="demo__list__item">
+          <p>Dark Theme Dropdown :</p>
+          <Dropdown theme="dark" />
+        </li>
+        <li className="demo__list__item">
+          <p>Custom Style Dropdown (inline style)</p>
+          <Dropdown
+            styles={{
+              dropdown: {
+                width: "400px",
+              },
+              dropdownSelected: {
+                backgroundColor: "green",
+              },
+              dropdownSelectedText: {
+                color: "orange",
+              },
+              dropdownArrow: {
+                border: "solid orange",
+                borderWidth: "0 3px 3px 0",
+              },
+              dropdownList: {
+                backgroundColor: "green",
+              },
+              dropdownItem: {
+                color: "white",
+              },
+            }}
+          />
+        </li>
+      </ul>
+      <h2>Installation</h2>
+      <pre className="code-container">
+        <code className="code">
+          {`npm install @baptistej24/react-dropdown
+or 
+yarn add @baptistej24/react-dropdown
+         
+
+import Dropdown from "@baptistej24/react-dropdown";`}
+        </code>
+      </pre>
+      <h2>Props</h2>
+      <table className="table">
+        <thead className="table__header">
+          <tr className="table__header__row">
+            <th className="table__header__cell">Prop</th>
+            <th className="table__header__cell">Type</th>
+            <th className="table__header__cell">Default</th>
+            <th className="table__header__cell">Description</th>
+          </tr>
+        </thead>
+        <tbody className="table__body">
+          <tr className="table__body__row">
+            <td className="table__body__cell">data</td>
+            <td className="table__body__cell">array</td>
+            <td className="table__body__cell">
+              [&quot;Option 1&quot;, &quot;Option 2&quot;, &quot;Option 3&quot;]
+            </td>
+            <td className="table__body__cell">Array of options</td>
+          </tr>
+          <tr className="table__body__row">
+            <td className="table__body__cell">placeholder</td>
+            <td className="table__body__cell">string</td>
+            <td className="table__body__cell">
+              &quot;Select an option...&quot;
+            </td>
+            <td className="table__body__cell">Placeholder text</td>
+          </tr>
+          <tr className="table__body__row">
+            <td className="table__body__cell">theme</td>
+            <td className="table__body__cell">
+              string (&quot;light&quot; || &quot;dark&quot;)
+            </td>
+            <td className="table__body__cell">&quot;light&quot;</td>
+            <td className="table__body__cell">Theme of the dropdown</td>
+          </tr>
+          <tr className="table__body__row">
+            <td className="table__body__cell">styles</td>
+            <td className="table__body__cell">object</td>
+            <td className="table__body__cell">
+              <p>
+                {"{"}&nbsp;dropdown: {"{}"},
+              </p>
+              <p>&nbsp;&nbsp;dropdownSelected: {"{}"},</p>
+              <p>&nbsp;&nbsp;dropdownSelectedText: {"{}"},</p>
+              <p>&nbsp;&nbsp;dropdownArrow: {"{}"},</p>
+              <p>&nbsp;&nbsp;dropdownList: {"{}"},</p>
+              <p>&nbsp;&nbsp;dropdownItem: {"{} }"}</p>
+            </td>
+            <td className="table__body__cell">
+              Inline styles (refer to css styles)
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h2>Usage</h2>
+      <pre className="code-container">
+        <code className="code">
+          {`import Dropdown from "@baptistej24/react-dropdown";
+
+// Basic Dropdown
+<Dropdown />
+
+// Custom placeholder Dropdown
+<Dropdown placeholder={"Custom Placeholder"} />
+
+// Custom data Dropdown
+<Dropdown data={["Value 1", "Value 2", "Value 3"]} />
+
+// Dark Theme Dropdown
+<Dropdown theme="dark" />
+
+// Custom Style Dropdown
+<Dropdown
+  styles={{
+    dropdown: {
+      width: "400px",
+    },
+    dropdownSelected: {
+      backgroundColor: "green",
+    },
+    dropdownSelectedText: {
+      color: "orange",
+    },
+    dropdownArrow: {
+      border: "solid orange",
+      borderWidth: "0 3px 3px 0",
+    },
+    dropdownList: {
+      backgroundColor: "green",
+    },
+    dropdownItem: {
+      color: "white",
+    },
+  }}
+/>
+            `}
+        </code>
+      </pre>
     </div>
   );
 }
--- src/components/dropdown.jsx
+++ src/components/dropdown.jsx
@@ -1,52 +1,119 @@
 import PropTypes from "prop-types";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import "./../styles/dropdown.css";
 
-const Dropdown = ({ data, placeholder, classname, defaultclassname }) => {
+const Dropdown = ({ data, placeholder, theme, styles }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selected, setSelected] = useState(null);
+  const dropdownRef = useRef(null);
 
-  const toggleDropdown = () => setIsOpen(!isOpen);
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
   const handleItemClick = (item) => {
     setSelected(item);
     setIsOpen(false);
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
   return (
     <div
-      className={
-        defaultclassname ? `lib-react-dropdown ${classname}` : classname
-      }
+      ref={dropdownRef}
+      className={`bj-react-dropdown`}
+      aria-expanded={isOpen}
+      aria-haspopup="listbox"
+      aria-label="Toggle dropdown"
+      data-theme={theme}
+      style={styles.dropdown}
     >
-      <div onClick={toggleDropdown}>
-        <span>{selected ? selected : placeholder}</span>
-        <span>{isOpen ? "▲" : "▼"}</span>
+      <div
+        className="dropdown__selected"
+        aria-labelledby="dropdown__selected"
+        onClick={toggleDropdown}
+        style={styles.dropdownSelected}
+      >
+        <span
+          id="dropdown__selected"
+          className="dropdown__selected__text"
+          style={styles.dropdownSelectedText}
+        >
+          {selected ? selected : placeholder}
+        </span>
+        <span>
+          <span
+            className={`${isOpen ? "arrow-open" : "arrow-close"}`}
+            aria-hidden="true"
+            style={styles.dropdownArrow}
+          ></span>
+          <span className="sr-only">
+            {isOpen ? "Close dropdown" : "Open dropdown"}
+          </span>
+        </span>
       </div>
-      {isOpen && (
-        <ul>
-          {data.map((item) => (
-            <li key={item} onClick={handleItemClick}>
-              {item}
-            </li>
-          ))}
-        </ul>
-      )}
+
+      <ul
+        id="dropdown__list"
+        className={`${isOpen ? "dropdown__list" : "sr-only"}`}
+        role="listbox"
+        aria-labelledby="dropdown__selected"
+        style={styles.dropdownList}
+      >
+        {data.map((item, index) => (
+          <li
+            key={index + item}
+            onClick={() => handleItemClick(item)}
+            tabIndex="0"
+            role="option"
+            aria-label={item}
+            className="dropdown__item"
+            data-selected={selected === item}
+            style={styles.dropdownItem}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
 Dropdown.defaultProps = {
   data: ["Option 1", "Option 2", "Option 3"],
-  placeholder: "Select an option",
-  classname: "",
-  defaultclassname: true,
+  placeholder: "Select an option...",
+  theme: "light",
+  styles: {
+    dropdown: {},
+    dropdownSelected: {},
+    dropdownSelectedText: {},
+    dropdownArrow: {},
+    dropdownList: {},
+    dropdownItem: {},
+  },
 };
 
 Dropdown.propTypes = {
   data: PropTypes.arrayOf(PropTypes.string),
   placeholder: PropTypes.string,
-  classname: PropTypes.string,
-  defaultclassname: PropTypes.bool,
+  theme: PropTypes.oneOf(["light", "dark"]),
+  styles: PropTypes.shape({
+    dropdown: PropTypes.object,
+    dropdownSelected: PropTypes.object,
+    dropdownSelectedText: PropTypes.object,
+    dropdownArrow: PropTypes.object,
+    dropdownList: PropTypes.object,
+    dropdownItem: PropTypes.object,
+  }),
 };
 
 export default Dropdown;
